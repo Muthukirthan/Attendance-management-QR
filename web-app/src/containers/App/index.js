3eb08eebdac33ca138b424c09e9e436c10d1a455
Muthu kirthan <kmkirthan@gmail.com>
2021-04-23 11:34:27 +0530

    Added new component to handle 404 pages (catch all routes)

--- web-app/src/containers/App/index.js
+++ web-app/src/containers/App/index.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Switch} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 import Signup from '../../components/Signup'
 import Registered from '../../components/Registered'
 import FacultyDashboard from '../../components/FacultyDashboard'
 import Attendance from '../../components/Attendance'
+import NoMatch from '../../components/NoMatch'
 import logo from '../../assets/qr.jpg'
 
 const app = () => {
@@ -16,10 +17,13 @@ const app = () => {
         <img src={logo} alt="qr logo" className="QrLogo"/><br/>
         <label className="AppName">QR Attendance Management</label>
       </div>
-      <Route exact path='/' component={Signup} />
-      <Route exact path={'/key/:roll_num(\\d+)?'} component={Registered} />
-      <Route exact path='/facultyDashboard' component={FacultyDashboard} />
-      <Route exact path='/attendance/:date/:key' component={Attendance} />
+      <Switch>
+        <Route exact path='/' component={Signup} />
+        <Route exact path={'/key/:roll_num(\\d+)?'} component={Registered} />
+        <Route exact path='/facultyDashboard' component={FacultyDashboard} />
+        <Route exact path='/attendance/:date/:key' component={Attendance} />
+        <Route component={NoMatch} />
+      </Switch>
     </BrowserRouter>
   )
 }
